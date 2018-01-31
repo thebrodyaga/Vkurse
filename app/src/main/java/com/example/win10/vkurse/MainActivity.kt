681f2dab4fba10023352f277f282676168c520c9
WILDBERRIES\Emelyanov.n4 <Emelyanov.n4@wildberries.ru>
2018-01-31 19:00:40 +0300

    navigation

--- app/src/main/java/com/example/win10/vkurse/MainActivity.kt
+++ app/src/main/java/com/example/win10/vkurse/MainActivity.kt
@@ -3,17 +3,24 @@ package com.example.win10.vkurse
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.CoordinatorLayout
 import android.support.design.widget.NavigationView
+import android.support.graphics.drawable.VectorDrawableCompat
+import android.support.v4.content.res.ResourcesCompat
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import com.example.win10.vkurse.R.id.*
+import com.example.win10.vkurse.dummy.DummyContent
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.toolbar.*
 
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
+        BottomNavigationView.OnNavigationItemSelectedListener, ItemFragment.OnListFragmentInteractionListener {
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem) {
+    }
 
 
     @SuppressLint("SetTextI18n")
@@ -21,45 +28,34 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-       /* (bottom_nav.layoutParams as CoordinatorLayout.LayoutParams)
-                .behavior = BottomNavigationViewBehavior()*/
+        (bottom_nav.layoutParams as CoordinatorLayout.LayoutParams)
+                .behavior = BottomNavigationViewBehavior()
         nvView.setNavigationItemSelectedListener(this)
-      //  bottom_nav.setOnNavigationItemSelectedListener(this)
-
-      //  supportFragmentManager.beginTransaction().replace(R.id.flContent, BlankFragment()).commit()
-        textView.text = "Start: \n-Line2\n-Line3 \n" +
-                "-Line2\n" +
-                "-Line3\n" +
-                "-Line2\n" +
-                "-Line3\n" +
-                "-Line2\n" +
-                "-Line3\n" +
-                "-Line2\n" +
-                "-Line3\n" +
-                "-Line2\n" +
-                "-Line3\n" +
-                "-Line2\n" +
-                "-Line3\n" +
-                "-Line2\n" +
-                "-Line3\n" +
-                "-Line2\n" +
-                "-Line3\n" +
-                "-Line2\n" +
-                "-Line3\n" +
-                "-Line2\n" +
-                "-Line3\n" +
-                "-Line2\n" +
-                "-Finish"
+        bottom_nav.setOnNavigationItemSelectedListener(this)
+        if (supportActionBar != null) {
+            val indicator = VectorDrawableCompat.create(resources, R.drawable.ic_menu, theme)
+            indicator?.setTint(ResourcesCompat.getColor(resources, R.color.white, theme))
+            supportActionBar?.setHomeAsUpIndicator(indicator)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer,ItemFragment()).commit()
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId) {
-            home -> {
+            android.R.id.home -> {
                 drawer_layout.openDrawer(GravityCompat.START)
+            }
+            toolbar_settings -> {
                 return true
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -73,13 +69,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             nav_third_fragment -> {
                 Toast.makeText(this, "third", Toast.LENGTH_LONG).show()
             }
-            action_map -> {
+            bottom_map -> {
                 Toast.makeText(this, "map", Toast.LENGTH_LONG).show()
             }
-            action_dial -> {
+            bottom_dial -> {
                 Toast.makeText(this, "dial", Toast.LENGTH_LONG).show()
             }
-            action_mail -> {
+            bottom_mail -> {
                 Toast.makeText(this, "mail", Toast.LENGTH_LONG).show()
             }
         }
