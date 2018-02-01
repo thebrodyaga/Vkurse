87fe58d1650c50421a36b362c28b97fed075b28f
WILDBERRIES\Emelyanov.n4 <Emelyanov.n4@wildberries.ru>
2018-02-01 14:53:14 +0300

    bottom navigation

--- app/src/main/java/com/example/win10/vkurse/MainActivity.kt
+++ app/src/main/java/com/example/win10/vkurse/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.win10.vkurse
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.design.widget.CoordinatorLayout
 import android.support.design.widget.NavigationView
 import android.support.graphics.drawable.VectorDrawableCompat
 import android.support.v4.content.res.ResourcesCompat
@@ -13,32 +11,28 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import com.example.win10.vkurse.R.id.*
-import com.example.win10.vkurse.dummy.DummyContent
+import com.roughike.bottombar.OnTabReselectListener
+import com.roughike.bottombar.OnTabSelectListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
-        BottomNavigationView.OnNavigationItemSelectedListener, ItemFragment.OnListFragmentInteractionListener {
-    override fun onListFragmentInteraction(item: DummyContent.DummyItem) {
-    }
-
+        OnTabSelectListener, OnTabReselectListener {
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        (bottom_nav.layoutParams as CoordinatorLayout.LayoutParams)
-                .behavior = BottomNavigationViewBehavior()
         nvView.setNavigationItemSelectedListener(this)
-        bottom_nav.setOnNavigationItemSelectedListener(this)
+        bottomBar.setOnTabSelectListener(this)
+        bottomBar.setOnTabReselectListener(this)
         if (supportActionBar != null) {
             val indicator = VectorDrawableCompat.create(resources, R.drawable.ic_menu, theme)
             indicator?.setTint(ResourcesCompat.getColor(resources, R.color.white, theme))
             supportActionBar?.setHomeAsUpIndicator(indicator)
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
-        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer,ItemFragment()).commit()
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -69,20 +63,40 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             nav_third_fragment -> {
                 Toast.makeText(this, "third", Toast.LENGTH_LONG).show()
             }
-            bottom_map -> {
-                Toast.makeText(this, "map", Toast.LENGTH_LONG).show()
-            }
-            bottom_dial -> {
-                Toast.makeText(this, "dial", Toast.LENGTH_LONG).show()
-            }
-            bottom_mail -> {
-                Toast.makeText(this, "mail", Toast.LENGTH_LONG).show()
-            }
         }
         item.isChecked = true
         title = item.title
         drawer_layout.closeDrawers()
         return true
     }
+
+    override fun onTabReSelected(tabId: Int) {
+        when (tabId) {
+            bottom_map -> {
+
+            }
+            bottom_dial -> {
+
+            }
+            bottom_mail -> {
+
+            }
+        }
+    }
+
+    override fun onTabSelected(tabId: Int) {
+        when (tabId) {
+            bottom_map -> {
+
+            }
+            bottom_dial -> {
+
+            }
+            bottom_mail -> {
+
+            }
+        }
+    }
+
 
 }
